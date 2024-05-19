b7eb06e97d35383f762cd362ce021ddffb7eae1e
VuTraMy66 <153701476+VuTraMy66@users.noreply.github.com>
2024-05-19 21:15:33 +0700

    Connect database

--- backend/server.js
+++ backend/server.js
@@ -1,33 +1,43 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const mssql = require('mssql');
+const sql = require('mssql');
 
 const app = express();
-const PORT = process.env.PORT || 3000; 
-
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(bodyParser.json());
 
+const config = {
+    user: 'sa',
+    password: '123456',
+    server: 'LAPTOPCUATUI',
+    database: 'LerryLazzyShop',
+    options: {
+        encrypt: true,
+        trustServerCertificate: true,
+    },
+};
+
+app.get('/api/products', async (req, res) => {
+    try {
+        let pool = await sql.connect(config);
+        let products = await pool.request().query('SELECT * FROM dbo.Products');
+        res.json(products.recordset);
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Server Error');
+    }
+});
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
 
-const config = {
-    user: 'sa',
-    password: '123456',
-    server: 'LAPTOPCUATUI\\SQLEXPRESS',
-    database: 'LerryLazzyShop',
-    options: {
-      encrypt: true, 
-      enableArithAbort: true,
-    },
-  };
-  
-  mssql.connect(config, (err) => {
+sql.connect(config, (err) => {
     if (err) {
-      console.error('Error connecting to SQL Server:', err);
-      return;
+        console.error('Error connecting to SQL Server:', err);
+        return;
     }
     console.log('Connected to SQL Server');
-  });
+});
