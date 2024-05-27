d605b8618370951f9745b9f53cc9d4cfb7488fd7
VuTraMy66 <153701476+VuTraMy66@users.noreply.github.com>
2024-05-27 21:32:49 +0700

    Complete login with API

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -1,57 +1,16 @@
 import axios from 'axios';
-import config from './config';
 
 const instance = axios.create({
-    baseURL: config.api.REACT_APP_API_BASE_URL,
+    baseURL: process.env.REACT_APP_API_BASE_URL,
     withCredentials: true
 });
-
-// Error constructor
-const createError = (httpStatusCode, statusCode, errorMessage, problems, errorCode = '') => {
-    const error = new Error();
-    error.httpStatusCode = httpStatusCode;
-    error.statusCode = statusCode;
-    error.errorMessage = errorMessage;
-    error.problems = problems;
-    error.errorCode = errorCode + "";
-    return error;
-};
-
-// Function to check the success status code
-export const isSuccessStatusCode = (s) => {
-    const statusType = typeof s;
-    return (statusType === 'number' && s === 0) || (statusType === 'string' && s.toUpperCase() === 'OK');
-};
 
 // Set up interceptor for axios
 instance.interceptors.response.use(
     (response) => {
         const { data } = response;
-        if (data.hasOwnProperty('s') && !isSuccessStatusCode(data['s']) && data.hasOwnProperty('errmsg')) {
-            return Promise.reject(createError(response.status, data['s'], data['errmsg'], null, data['errcode'] ? data['errcode'] : ""));
-        }
-        if (data.hasOwnProperty('s') && data.hasOwnProperty('d')) {
-            return data['d'];
-        }
-        if (data.hasOwnProperty('s') && Object.keys(data).length === 1) {
-            return null;
-        }
         return response.data;
     },
-    (error) => {
-        const { response } = error;
-        if (response == null) {
-            return Promise.reject(error);
-        }
-        const { data } = response;
-        if (data.hasOwnProperty('s') && data.hasOwnProperty('errmsg')) {
-            return Promise.reject(createError(response.status, data['s'], data['errmsg']));
-        }
-        if (data.hasOwnProperty('code') && data.hasOwnProperty('message')) {
-            return Promise.reject(createError(response.status, data['code'], data['message'], data['problems']));
-        }
-        return Promise.reject(createError(response.status));
-    }
 );
 
 export default instance;
--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -1,116 +1,146 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { handleLoginApi } from '../services/userService';
+import * as actions from "../store/actions";
 import './LoginPage.css';
-import { handleLoginApi }  from '../services/userService';
 
 class LoginPage extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            username: '',
-            password: '',
-            showPassword: false
-        };
-    }
-
-    handleLogin = async (event) => {
-        event.preventDefault();
-        console.log(this.state.username, this.state.password);
-        console.log("All state: ", this.state);
-
-        try {
-            await handleLoginApi(this.state.username, this.state.password);
-        } catch (error) {
-            console.log(error);
-        }
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: '',
+      password: '',
+      showPassword: false,
+      errMessage: ''
     };
+  }
 
-    handleOnChangeInput = (event) => {
-        const { name, value } = event.target;
-        console.log(value);
+  handleLogin = async (event) => {
+    event.preventDefault();
+    this.setState({
+      errMessage: '',
+    });
+    try {
+      let data = await handleLoginApi(this.state.username, this.state.password);
+      if (data && data.errCode !== 0) {
         this.setState({
-            [name]: value
+          errMessage: data.message,
         });
+      } else if (data && data.errCode === 0) {
+        // this.props.userLoginSuccess(data.user);
+        console.log("Login succeed!")
+      }
+    } catch (error) {
+      if (error.response) {
+        if (error.response.data) {
+          this.setState({
+            errMessage: error.response.data.message
+          });
+        }
+      }
+      console.log(error.response);
     }
+  };
 
-    handleShowPassword = () => {
-        this.setState((prevState) => ({
-            showPassword: !prevState.showPassword
-        }));
-    }
+  handleOnChangeInput = (event) => {
+    const { name, value } = event.target;
+    console.log(value);
+    this.setState({
+      [name]: value
+    });
+  }
 
-    render() {
-        const { username, password, showPassword } = this.state;
+  handleShowPassword = () => {
+    this.setState((prevState) => ({
+      showPassword: !prevState.showPassword
+    }));
+  }
 
-        return (
-            <section className="vh-100 gradient-custom">
-                <div className="container py-5 h-100">
-                    <div className="row d-flex justify-content-center align-items-center h-100">
-                        <div className="col-12 col-md-8 col-lg-6 col-xl-5">
-                            <div className="card bg-dark text-white" style={{ borderRadius: '1rem' }}>
-                                <div className="card-body p-5 text-center">
-                                    <div className="mb-md-5 mt-md-4 pb-5">
-                                        <h2 className="fw-bold mb-2 text-uppercase">Login</h2>
-                                        <p className="text-white-50 mb-5">Please enter your account and password!</p>
+  render() {
+    const { username, password, showPassword } = this.state;
 
-                                        <form onSubmit={this.handleLogin}>
-                                            <div className="form-outline form-white mb-4">
-                                                <input 
-                                                    type="text" 
-                                                    id="typeUsername" 
-                                                    name="username" 
-                                                    placeholder="Username" 
-                                                    className="form-control form-control-lg" 
-                                                    value={username} 
-                                                    onChange={this.handleOnChangeInput}
-                                                />
-                                            </div>
+    return (
+      <section className="vh-100 gradient-custom">
+        <div className="container py-5 h-100">
+          <div className="row d-flex justify-content-center align-items-center h-100">
+            <div className="col-12 col-md-8 col-lg-6 col-xl-5">
+              <div className="card bg-dark text-white" style={{ borderRadius: '1rem' }}>
+                <div className="card-body p-5 text-center">
+                  <div className="mb-md-5 mt-md-4 pb-5">
+                    <h2 className="fw-bold mb-2 text-uppercase">Login</h2>
+                    <p className="text-white-50 mb-5">Please enter your account and password!</p>
 
-                                            <div className="form-outline form-white mb-4 position-relative">
-                                                <input 
-                                                    type={this.state.showPassword ? "text" : "password"} 
-                                                    id="typePassword" 
-                                                    name="password" 
-                                                    placeholder="Password" 
-                                                    className="form-control form-control-lg" 
-                                                    value={password} 
-                                                    onChange={this.handleOnChangeInput}
-                                                />
-                                                <i
-                                                    className={"fa " + (showPassword ? "fa-eye" : "fa-eye-slash") + " position-absolute"}
-                                                    style={{ top: '50%', right: '10px', transform: 'translateY(-50%)', cursor: 'pointer', opacity: '0.7'}}
-                                                    onClick={this.handleShowPassword}
-                                                ></i>
-                                            </div>
+                    <form onSubmit={this.handleLogin}>
+                      <div className="form-outline form-white mb-4">
+                        <input 
+                          type="text" 
+                          id="typeUsername" 
+                          name="username" 
+                          placeholder="Username" 
+                          className="form-control form-control-lg" 
+                          value={username} 
+                          onChange={this.handleOnChangeInput}
+                        />
+                      </div>
 
-                                            <div className="d-flex justify-content-between mb-4">
-                                                <div className="remember-me">
-                                                    <input id="remember" type="checkbox"/>
-                                                    <label>Remember me</label>
-                                                </div>
-                                                <p className="small mb-5 pb-lg-2"><a className="text-white-50" href="#!">Forgot password?</a></p>
-                                            </div>
-                                            
-                                            <button className="btn btn-outline-light btn-lg px-5" type="submit">Login</button>
-                                        </form>
+                      <div className="form-outline form-white mb-4 position-relative">
+                        <input 
+                          type={this.state.showPassword ? "text" : "password"} 
+                          id="typePassword" 
+                          name="password" 
+                          placeholder="Password" 
+                          className="form-control form-control-lg" 
+                          value={password} 
+                          onChange={this.handleOnChangeInput}
+                        />
+                        <i
+                          className={"fa " + (showPassword ? "fa-eye" : "fa-eye-slash") + " position-absolute"}
+                          style={{ top: '50%', right: '10px', transform: 'translateY(-50%)', cursor: 'pointer', opacity: '0.7'}}
+                          onClick={this.handleShowPassword}
+                        ></i>
+                      </div>
 
-                                        <div className="d-flex justify-content-center text-center mt-4 pt-1">
-                                            <a href="#!" className="text-white"><i className="fa fa-facebook-f fa-lg"></i></a>
-                                            <a href="#!" className="text-white"><i className="fa fa-twitter fa-lg mx-4 px-2"></i></a>
-                                            <a href="#!" className="text-white"><i className="fa fa-google fa-lg"></i></a>
-                                        </div>
-                                    </div>
-
-                                    <div>
-                                        <p className="mb-0">Don't have an account? <a href="#!" className="text-white-50 fw-bold">Sign Up</a></p>
-                                    </div>
-                                </div>
-                            </div>
+                      <div className="d-flex justify-content-between mb-4">
+                        <div className="remember-me">
+                          <input id="remember" type="checkbox"/>
+                          <label>Remember me</label>
                         </div>
+                        <p className="small mb-2 pb-lg-2"><a className="text-white-50" href="#!">Forgot password?</a></p>
+                      </div>
+                      
+                      <div className="form-outline mb-3 error-message">
+                        {this.state.errMessage}
+                      </div>
+                      
+                      <button className="btn btn-outline-light btn-lg px-5" type="submit">Login</button>
+                    </form>
+
+                    <div className="d-flex justify-content-center text-center mt-4 pt-1">
+                      <a href="#!" className="text-white"><i className="fa fa-facebook-f fa-lg"></i></a>
+                      <a href="#!" className="text-white"><i className="fa fa-twitter fa-lg mx-4 px-2"></i></a>
+                      <a href="#!" className="text-white"><i className="fa fa-google fa-lg"></i></a>
                     </div>
+                  </div>
+
+                  <div>
+                    <p className="mb-0">Don't have an account? <a href="#!" className="text-white-50 fw-bold">Sign Up</a></p>
+                  </div>
                 </div>
-            </section>
-        );
-    }
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    );
+  }
 }
 
-export default LoginPage;
+// const mapDispatchToProps = dispatch => {
+//   return {
+//     adminLoginSuccess: (adminInfo) => dispatch(actions.adminLoginSuccess(adminInfo)),
+//     adminLoginFail: () => dispatch(actions.adminLoginFail()),
+//     userLoginSuccess: (userInfo) => dispatch(actions.userLoginSuccess(userInfo)),
+//   };
+// };
+
+export default (LoginPage);
